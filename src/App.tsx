4a7e7c28f633e531b1b6217c84406c1e9aec2a3d
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-08 14:02:59 +0900

    Call wasm code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div>
       <h1 className="text-red-500">Hello</h1>
+      <canvas id="canvas" />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import init, { start } from "../wasm/pkg/wasm";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
+init().then(() => {
+  start();
+});
