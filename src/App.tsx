f9602236b53c7a2c8a7282c477597ad4d23c7415
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-08 15:16:24 +0900

    Render text

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ function App() {
   return (
     <div>
       <h1 className="text-red-500">Hello</h1>
-      <canvas id="canvas" />
+      <canvas
+        id="canvas"
+        width="2000"
+        height="200"
+        className="w-[1000px] h-[100px] border-2 border-black"
+      />
     </div>
   );
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -9,8 +9,6 @@ pub fn start() {
     log::set_logger(&DEFAULT_LOGGER).unwrap();
     log::set_max_level(log::LevelFilter::Info);
 
-    let bytes = include_bytes!("NotoSansJP-Regular.otf");
-
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
@@ -25,30 +23,73 @@ pub fn start() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.begin_path();
-
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
-
     info!("{canvas:?}");
+
+    draw_text(&context, "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。");
+}
+
+struct TestOutlineBuilder<'a> {
+    context: &'a web_sys::CanvasRenderingContext2d,
+}
+
+impl<'a> ttf_parser::OutlineBuilder for TestOutlineBuilder<'a> {
+    fn move_to(&mut self, x: f32, y: f32) {
+        self.context.move_to(x as f64, y as f64);
+    }
+
+    fn line_to(&mut self, x: f32, y: f32) {
+        self.context.line_to(x as f64, y as f64);
+    }
+
+    fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
+        self.context
+            .quadratic_curve_to(x1 as f64, y1 as f64, x as f64, y as f64);
+    }
+
+    fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
+        self.context.bezier_curve_to(
+            x1 as f64, y1 as f64, x2 as f64, y2 as f64, x as f64, y as f64,
+        );
+    }
+
+    fn close(&mut self) {
+        self.context.close_path();
+    }
+}
+
+fn draw_text(context: &web_sys::CanvasRenderingContext2d, text: &str) {
+    let font_data = include_bytes!("NotoSansJP-Regular.otf");
+
+    let face = ttf_parser::Face::from_slice(font_data, 0).unwrap();
+    info!("face: {}", face.number_of_glyphs());
+
+    let mut rustybuzz_face = rustybuzz::Face::from_slice(font_data, 0).unwrap();
+    let mut buffer = rustybuzz::UnicodeBuffer::new();
+    buffer.push_str(&text);
+
+    let glyph_buffer = rustybuzz::shape(&rustybuzz_face, &[], buffer);
+
+    info!(
+        "{}",
+        glyph_buffer.serialize(&rustybuzz_face, rustybuzz::SerializeFlags::default())
+    );
+
+    context.translate(0.0, 100.0);
+    context.scale(0.05, -0.05);
+
+    for i in 0..glyph_buffer.len() {
+        let glyph = glyph_buffer.glyph_infos()[i];
+        let pos = glyph_buffer.glyph_positions()[i];
+
+        info!("{:?}", pos);
+
+        face.outline_glyph(
+            ttf_parser::GlyphId(glyph.glyph_id as u16),
+            &mut TestOutlineBuilder { context },
+        );
+
+        context.fill();
+
+        context.translate(pos.x_advance as f64, 0.0);
+    }
 }
