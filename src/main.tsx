86bf2e97db16c3118f14ff92fe37f8b81602adfa
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-09 17:13:24 +0900

    Use TextRenderer from JS

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import init, { start } from "../wasm/pkg/wasm";
+import init, { start, TextRenderer } from "../wasm/pkg/wasm";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -13,4 +13,15 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
 init().then(() => {
   start();
+
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const context = canvas.getContext("2d")!;
+
+  const renderer = new TextRenderer(context);
+
+  const text =
+    "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n" +
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+  renderer.draw_text(text, 32, 48, 32, 32, 320);
 });
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -11,25 +11,25 @@ pub fn start() {
     log::set_logger(&DEFAULT_LOGGER).unwrap();
     log::set_max_level(log::LevelFilter::Info);
 
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
+    // let document = web_sys::window().unwrap().document().unwrap();
+    // let canvas = document.get_element_by_id("canvas").unwrap();
+    // let canvas: web_sys::HtmlCanvasElement = canvas
+    //     .dyn_into::<web_sys::HtmlCanvasElement>()
+    //     .map_err(|_| ())
+    //     .unwrap();
 
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
+    // let context = canvas
+    //     .get_context("2d")
+    //     .unwrap()
+    //     .unwrap()
+    //     .dyn_into::<web_sys::CanvasRenderingContext2d>()
+    //     .unwrap();
 
-    info!("{canvas:?}");
+    // info!("{canvas:?}");
 
-    let text = "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n\
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    // let text = "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n\
+    //     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
-    let text_renderer = TextRenderer::new(context);
-    text_renderer.draw_text(text, 32.0, 48.0, 32.0, 32.0, 320.0);
+    // let text_renderer = TextRenderer::new(context);
+    // text_renderer.draw_text(text, 32.0, 48.0, 32.0, 32.0, 320.0);
 }
