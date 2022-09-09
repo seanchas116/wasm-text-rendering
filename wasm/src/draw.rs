b620a59380d53926e06d4a64c94bf7b86d32d91e
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-09 14:45:36 +0900

    Refactor

--- wasm/src/draw.rs
+++ wasm/src/draw.rs
@@ -47,80 +47,96 @@ impl<'a> ttf_parser::OutlineBuilder for TestOutlineBuilder<'a> {
     }
 }
 
-pub fn draw_text(
-    context: &web_sys::CanvasRenderingContext2d,
-    text: &str,
-    font_size: f64,
-    line_height: f64,
-    x: f64,
-    y: f64,
-    width: f64,
-) {
-    let font_data = include_bytes!("NotoSansJP-Regular.otf");
+pub struct TextRenderer<'a> {
+    context: &'a web_sys::CanvasRenderingContext2d,
+    font_face: ttf_parser::Face<'a>,
+    rustybuzz_face: rustybuzz::Face<'a>,
+}
 
-    let face = ttf_parser::Face::from_slice(font_data, 0).unwrap();
-
-    let scale = font_size / face.units_per_em() as f64;
-
-    let rustybuzz_face = rustybuzz::Face::from_slice(font_data, 0).unwrap();
-
-    context.translate(x, y);
-
-    let linebreaks: Vec<(usize, unicode_linebreak::BreakOpportunity)> =
-        unicode_linebreak::linebreaks(text).collect();
-
-    let mut pos_x = 0.0;
-    let mut pos_y = 0.0;
-
-    let mut last_offset: usize = 0;
-    for (offset, break_type) in linebreaks {
-        let mut span = &text[last_offset..offset];
-        last_offset = offset;
-        if span.ends_with("\n") {
-            span = &span[..span.len() - 1];
+impl<'a> TextRenderer<'a> {
+    pub fn new(context: &'a web_sys::CanvasRenderingContext2d) -> TextRenderer {
+        let font_data = include_bytes!("NotoSansJP-Regular.otf");
+        let font_face = ttf_parser::Face::from_slice(font_data, 0).unwrap();
+        let rustybuzz_face = rustybuzz::Face::from_slice(font_data, 0).unwrap();
+        TextRenderer {
+            context,
+            font_face,
+            rustybuzz_face,
         }
-        info!("span: {:?}", span);
+    }
 
-        let mut buffer = rustybuzz::UnicodeBuffer::new();
-        buffer.push_str(span);
-        let glyph_buffer = rustybuzz::shape(&rustybuzz_face, &[], buffer);
+    pub fn draw_text(
+        &self,
+        text: &str,
+        font_size: f64,
+        line_height: f64,
+        x: f64,
+        y: f64,
+        width: f64,
+    ) {
+        let scale = font_size / self.font_face.units_per_em() as f64;
 
-        let span_width = glyph_buffer
-            .glyph_positions()
-            .iter()
-            .map(|info| info.x_advance)
-            .sum::<i32>() as f64
-            * scale;
+        self.context.translate(x, y);
 
-        if pos_x + span_width > width {
-            pos_x = 0.0;
-            pos_y += line_height;
-        }
+        let linebreaks: Vec<(usize, unicode_linebreak::BreakOpportunity)> =
+            unicode_linebreak::linebreaks(text).collect();
 
-        for i in 0..glyph_buffer.len() {
-            let glyph = glyph_buffer.glyph_infos()[i];
-            let pos = glyph_buffer.glyph_positions()[i];
+        let mut pos_x = 0.0;
+        let mut pos_y = 0.0;
 
-            context.save();
-            context.translate(pos_x, pos_y + font_size);
-            context.scale(1.0, -1.0);
+        let mut last_offset: usize = 0;
+        for (offset, break_type) in linebreaks {
+            let mut span = &text[last_offset..offset];
+            last_offset = offset;
+            if span.ends_with("\n") {
+                span = &span[..span.len() - 1];
+            }
+            info!("span: {:?}", span);
 
-            context.begin_path();
+            let mut buffer = rustybuzz::UnicodeBuffer::new();
+            buffer.push_str(span);
+            let glyph_buffer = rustybuzz::shape(&self.rustybuzz_face, &[], buffer);
 
-            face.outline_glyph(
-                ttf_parser::GlyphId(glyph.glyph_id as u16),
-                &mut TestOutlineBuilder { context, scale },
-            );
+            let span_width = glyph_buffer
+                .glyph_positions()
+                .iter()
+                .map(|info| info.x_advance)
+                .sum::<i32>() as f64
+                * scale;
 
-            context.fill();
-            context.restore();
+            if pos_x + span_width > width {
+                pos_x = 0.0;
+                pos_y += line_height;
+            }
 
-            pos_x += pos.x_advance as f64 * scale;
-        }
+            for i in 0..glyph_buffer.len() {
+                let glyph = glyph_buffer.glyph_infos()[i];
+                let pos = glyph_buffer.glyph_positions()[i];
 
-        if break_type == unicode_linebreak::BreakOpportunity::Mandatory {
-            pos_x = 0.0;
-            pos_y += line_height;
+                self.context.save();
+                self.context.translate(pos_x, pos_y + font_size);
+                self.context.scale(1.0, -1.0);
+
+                self.context.begin_path();
+
+                self.font_face.outline_glyph(
+                    ttf_parser::GlyphId(glyph.glyph_id as u16),
+                    &mut TestOutlineBuilder {
+                        context: self.context,
+                        scale,
+                    },
+                );
+
+                self.context.fill();
+                self.context.restore();
+
+                pos_x += pos.x_advance as f64 * scale;
+            }
+
+            if break_type == unicode_linebreak::BreakOpportunity::Mandatory {
+                pos_x = 0.0;
+                pos_y += line_height;
+            }
         }
     }
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,6 @@
 mod draw;
 
-use crate::draw::draw_text;
+use crate::draw::TextRenderer;
 use log::info;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -30,5 +30,6 @@ pub fn start() {
     let text = "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n\
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
-    draw_text(&context, text, 32.0, 48.0, 32.0, 32.0, 320.0);
+    let text_renderer = TextRenderer::new(&context);
+    text_renderer.draw_text(text, 32.0, 48.0, 32.0, 32.0, 320.0);
 }
