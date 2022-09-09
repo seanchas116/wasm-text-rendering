4a4c0f37d95c97714ed229417fa8273b58dfe3c9
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-09 14:17:51 +0900

    Split string into line break opportunities

--- wasm/src/draw.rs
+++ wasm/src/draw.rs
@@ -8,19 +8,19 @@ struct TestOutlineBuilder<'a> {
 
 impl<'a> ttf_parser::OutlineBuilder for TestOutlineBuilder<'a> {
     fn move_to(&mut self, x: f32, y: f32) {
-        info!("move_to: {}, {}", x, y);
+        //info!("move_to: {}, {}", x, y);
         self.context
             .move_to(x as f64 * self.scale, y as f64 * self.scale);
     }
 
     fn line_to(&mut self, x: f32, y: f32) {
-        info!("line_to: {}, {}", x, y);
+        //info!("line_to: {}, {}", x, y);
         self.context
             .line_to(x as f64 * self.scale, y as f64 * self.scale);
     }
 
     fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
-        info!("quad_to: {}, {}, {}, {}", x1, y1, x, y);
+        //info!("quad_to: {}, {}, {}, {}", x1, y1, x, y);
         self.context.quadratic_curve_to(
             x1 as f64 * self.scale,
             y1 as f64 * self.scale,
@@ -30,7 +30,7 @@ impl<'a> ttf_parser::OutlineBuilder for TestOutlineBuilder<'a> {
     }
 
     fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
-        info!("curve_to: {}, {}, {}, {}, {}, {}", x1, y1, x2, y2, x, y);
+        //info!("curve_to: {}, {}, {}, {}, {}, {}", x1, y1, x2, y2, x, y);
         self.context.bezier_curve_to(
             x1 as f64 * self.scale,
             y1 as f64 * self.scale,
@@ -42,16 +42,27 @@ impl<'a> ttf_parser::OutlineBuilder for TestOutlineBuilder<'a> {
     }
 
     fn close(&mut self) {
-        info!("close");
+        //info!("close");
         self.context.close_path();
     }
 }
 
 pub fn draw_text(context: &web_sys::CanvasRenderingContext2d, text: &str, size: f64) {
+    let linebreaks: Vec<(usize, unicode_linebreak::BreakOpportunity)> =
+        unicode_linebreak::linebreaks(text).collect();
+
+    let lines: Vec<Vec<&str>> = Vec::new();
+
+    let mut last_offset: usize = 0;
+    for (offset, break_type) in linebreaks {
+        let span = &text[last_offset..offset];
+        last_offset = offset;
+        info!("span: {}", span);
+    }
+
     let font_data = include_bytes!("NotoSansJP-Regular.otf");
 
     let face = ttf_parser::Face::from_slice(font_data, 0).unwrap();
-    info!("units per em: {}", face.units_per_em());
 
     let scale = size / face.units_per_em() as f64;
 
@@ -71,8 +82,6 @@ pub fn draw_text(context: &web_sys::CanvasRenderingContext2d, text: &str, size: 
         for i in 0..glyph_buffer.len() {
             let glyph = glyph_buffer.glyph_infos()[i];
             let pos = glyph_buffer.glyph_positions()[i];
-
-            info!("{:?}", pos);
 
             context.begin_path();
 
