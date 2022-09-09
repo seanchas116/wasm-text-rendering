fab8af321075ae44f9312af255c0b5e1f6c74e5f
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-09 17:52:34 +0900

    Render in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import React, { useRef } from "react";
+import { useEffect } from "react";
+import { TextRenderer } from "../wasm/pkg/wasm";
 function App() {
+  const canvasRef = React.createRef<HTMLCanvasElement>();
+  const renderer = useRef<TextRenderer>();
+
+  useEffect(() => {
+    const context = canvasRef.current?.getContext("2d")!;
+
+    renderer.current = new TextRenderer(context);
+
+    const text =
+      "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n" +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+    renderer.current.drawText(text, 32, 48, 32, 32, 320);
+  }, []);
+
   return (
     <div className="p-4">
       <canvas
-        id="canvas"
+        ref={canvasRef}
         width="2000"
         height="800"
         className="w-[1000px] h-[400px] border-2 border-black"
--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,29 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import init, { start, TextRenderer } from "../wasm/pkg/wasm";
+import initWasm, { start, TextRenderer } from "../wasm/pkg/wasm";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function init() {
+  await initWasm();
 
-init().then(() => {
   start();
 
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  const context = canvas.getContext("2d")!;
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 
-  const renderer = new TextRenderer(context);
+  // const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  // const context = canvas.getContext("2d")!;
 
-  const text =
-    "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n" +
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  // const renderer = new TextRenderer(context);
 
-  renderer.drawText(text, 32, 48, 32, 32, 320);
-});
+  // const text =
+  //   "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。\n" +
+  //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+  // renderer.drawText(text, 32, 48, 32, 32, 320);
+}
+
+init();
